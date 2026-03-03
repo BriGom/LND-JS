caa004932a3efb8d33a2f5b259f642f61be34e19
BriGom <brigomsua1411@gmail.com>
2026-03-03 16:46:31 +0000

    Añadido archivo index.html y mejorada la salida de consola en ad-5-2-1.js

--- Ejercicios clase/ad-5-2-1.js
+++ Ejercicios clase/ad-5-2-1.js
@@ -1,13 +1,19 @@
+
+// Contacto 1
 const nombre1 = "Maxwell Wright";
 const telefono1 = "(0191) 719 6495";
 const email1 = "curabitur.egestas.nunc@nonummyac.co.uk";
 
+// Contacto 2
 const nombre2 = "Raja Villareal";
 const telefono2 = "0866 398 2895";
 const email2 = "posuere.vulputate@sed.com";
 
+// Contacto 3
 const nombre3 = "Helen Richards";
 const telefono3 = "0800 1111";
 const email3 = "libero@convallis.edu";
 
-console.log(nombre1 + telefono1 + email1);
+console.log(nombre1 + "/" + telefono1 + "/" + email1);
+console.log(nombre2 + "/" + telefono2 + "/" + email2);
+console.log(nombre3 + "/" + telefono3 + "/" + email3);
